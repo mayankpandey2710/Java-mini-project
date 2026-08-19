cac17ad191221825c979a5f608ef72d43e44ed3a
Mayank Pandey <mayankpandey2710@gmail.com>
2026-08-19 08:24:32 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,23 @@
+package assignmentjava;
+public class Customer {
+
+    String name;
+    int cash;
+
+    public Customer(String name, int cash) {
+
+        this.name = name;
+        this.cash = cash;
+    }
+
+    public void buy(Smartphone sm1) {
+
+        System.out.println("Trying to buy the smartphone: " + sm1);
+    }
+
+    @Override
+    public String toString() {
+
+        return this.name;
+    }
+}
